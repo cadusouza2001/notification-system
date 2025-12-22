7f59d49cfffec0da89b2c0f506ca239f3e12da89
cadusouza2001 <cadusouza2001@gmail.com>
2025-12-22 00:49:53 -0300

    Update CI workflow and Cypress configuration for consistent server host

--- frontend/cypress.config.cjs
+++ frontend/cypress.config.cjs
@@ -13,7 +13,7 @@ module.exports = defineConfig({
     },
   },
   e2e: {
-    baseUrl: "http://localhost:5173",
+    baseUrl: "http://127.0.0.1:5173",
     setupNodeEvents(on, config) {
       const codeCoverageTask = require("@cypress/code-coverage/task");
       codeCoverageTask(on, config);
